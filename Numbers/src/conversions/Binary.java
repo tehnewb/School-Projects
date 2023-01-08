9bb10d6c621cd30bd4c227d4edbce1354c0026d0
tehnewb <albert_beaupre@hotmail.com>
2023-01-08 18:20:46 +0400

    Update Binary.java

--- Numbers/src/conversions/Binary.java
+++ Numbers/src/conversions/Binary.java
@@ -1,27 +1,34 @@
 package conversions;
 
+/**
+ * This class holds various methods for binary conversions.
+ * 
+ * @author Albert Beaupre
+ * @date January 8th, 2023
+ */
 public class Binary {
 
+	/**
+	 * Converts the given {@code binary} string into a 32-Bit IEEE 754
+	 * Single-Precision Floating Point binary value.
+	 * 
+	 * @param binary the string to convert
+	 * @return the converted string
+	 */
 	public static String convertToIEEE754(String binary) {
-		String[] split = binary.split("\\.");
-		String leftDecimal = split[0];
-		String rightDecimal = split[1];
-		boolean negative = leftDecimal.charAt(0) == '-';
+		String[] split = binary.split("\\."); // split the binary string by the decimal location
+		String leftDecimal = split[0]; // retrieve the left side of the decimal
+		String rightDecimal = split[1]; // retrieve the right side of the decimal
+		boolean negative = leftDecimal.charAt(0) == '-'; // flag true if the binary value is negative
 
-		int shifts = binary.indexOf('.') - 1;
-		int bias = 127;
+		int shifts = binary.indexOf('.') - 1; // the amount of shifts from the right
+		int bias = 127; // the constant bias value for 32-bit
 		int exponent = bias + shifts;
 
-		String sign = negative ? "1" : "0";
-		String exp = String.format("%1$8s", Integer.toBinaryString(exponent));
-		String fraction = String.format("%1$-23s", leftDecimal.substring(leftDecimal.indexOf('1') + 1) + rightDecimal).replace(" ", "0");
-
-		System.out.println("32-Bit IEEE 754 Single-Precision Floating Point Binary");
-		System.out.println("--\t--------\t-----------------------");
-		System.out.println("" + sign + ".\t" + exp + "\t" + fraction + "");
-		System.out.println("--\t--------\t-----------------------");
-
 		StringBuilder result = new StringBuilder();
+		result.append(negative ? '1' : '0'); // append 1 if negative value, otherwise append 0
+		result.append(String.format("%1$8s", Integer.toBinaryString(exponent))); // append exponent as binary
+		result.append(String.format("%1$-23s", leftDecimal.substring(leftDecimal.indexOf('1') + 1) + rightDecimal).replace(" ", "0")); // significand
 		return result.toString();
 	}
 
