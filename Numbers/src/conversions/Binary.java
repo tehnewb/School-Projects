4af4e14167f31d2b7d776d0c28868dc71856f0dc
tehnewb <albert_beaupre@hotmail.com>
2023-01-11 18:40:37 +0400

    Added 2's and 1's complement
    -Added flipBits(String) method
    -Added onesComplement(String) method
    -Added twosComplement(String) method
    -Added addBinary(String, String) method

--- Numbers/src/conversions/Binary.java
+++ Numbers/src/conversions/Binary.java
@@ -32,4 +32,83 @@ public class Binary {
 		return result.toString();
 	}
 
+	/**
+	 * Flips the bits of the given {@code binary} string.
+	 * 
+	 * @param binary the string of binary to flip
+	 * @return the flipped binary string
+	 */
+	public static String flipBits(String binary) {
+		char[] sequence = new char[binary.length()];
+		for (int i = 0; i < binary.length(); i++) {
+			if (binary.charAt(i) == '0') {
+				sequence[i] = '1';
+			} else {
+				sequence[i] = '0';
+			}
+		}
+		return new String(sequence);
+	}
+
+	/**
+	 * Converts the given {@code binary} string to the 1's complement form.
+	 * 
+	 * @param binary the string to convert
+	 * @return the 1's complement form of the given string
+	 */
+	public static String onesComplement(String binary) {
+		return flipBits(binary);
+	}
+
+	/**
+	 * Converts the given {@code binary} string to the 2's complement form.
+	 * 
+	 * @param binary the string to convert
+	 * @return the 2's complement form of the given string
+	 */
+	public static String twosComplement(String binary) {
+		return addBinary(flipBits(binary), "1");
+	}
+
+	/**
+	 * Adds both binary strings together and returns the added binary string.
+	 * 
+	 * @param first  the first binary string to add
+	 * @param second the second binary string to add
+	 * @return the added binary string
+	 */
+	public static String addBinary(String first, String second) {
+		boolean carried = false;
+		int length = Math.max(first.length(), second.length());
+		first = first.replace(" ", "");
+		second = second.replace(" ", "");
+
+		first = String.format("%1$" + length + "s", first).replace(" ", "0");
+		second = String.format("%1$" + length + "s", second).replace(" ", "0");
+		char[] sequence = new char[length];
+		for (int i = length - 1; i >= 0; i--) {
+			if (first.charAt(i) == '1' && second.charAt(i) == '1') {
+				if (carried) {
+					sequence[i] = '1';
+					carried = false;
+				} else {
+					sequence[i] = '0';
+				}
+				carried = true;
+			} else if ((first.charAt(i) == '0' && second.charAt(i) == '1') || (first.charAt(i) == '1' && second.charAt(i) == '0')) {
+				if (carried) {
+					sequence[i] = '0';
+				} else {
+					sequence[i] = '1';
+				}
+			} else if (carried) {
+				carried = false;
+				sequence[i] = '1';
+			} else {
+				sequence[i] = '0';
+			}
+		}
+		return new String(sequence);
+	}
+
 }
